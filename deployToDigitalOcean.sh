df771dfbd469169fe12798b395ee62ee38db3c57
Baljeet Sandhu <Baljeet.mail@gmail.com>
2016-09-10 14:43:32 -0400

    Add deploy script

--- deployToDigitalOcean.sh
+++ deployToDigitalOcean.sh
@@ -1,4 +1,13 @@
-ssh root@45.55.192.142 'mkdir ~/epitomicweb';
+echo "*** Deploying to DigitalOcean - assuming that SSH keys for CodeShip are added to target host ***"
+echo""
+
+ssh root@45.55.192.142 'mkdir ~/epitomicweb'
 scp * root@45.55.192.142:~/epitomicweb;
-scp -r server root@45.55.192.142:~/epitomicweb;
-scp -r site root@45.55.192.142:~/epitomicweb;
+scp -rC server root@45.55.192.142:~/epitomicweb;
+scp -rC site root@45.55.192.142:~/epitomicweb;
+
+echo""
+echo "*** Done ***"
+echo "Bouncing node"
+ssh root@45.55.192.142 kill $(ps -ef | grep '[s]erver/start.js' | awk '{print $2}')
+ssh root@45.55.192.142 'cd ~/epitomicweb;npm install;nohup nodejs ./server/start.js &'
