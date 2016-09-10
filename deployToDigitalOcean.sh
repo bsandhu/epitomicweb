1f464a64f1623410f3eddbf837e17e627ef205b7
Baljeet Sandhu <Baljeet.mail@gmail.com>
2016-09-10 16:11:44 -0400

    Add exit code

--- deployToDigitalOcean.sh
+++ deployToDigitalOcean.sh
@@ -16,3 +16,5 @@ ssh root@45.55.192.142 kill $(ps -ef | grep '[s]erver/start.js' | awk '{print $2
 echo""
 echo "*** Starting Node ***"
 ssh root@45.55.192.142 'cd ~/epitomicweb;npm install;nohup nodejs ./server/start.js &'
+
+exit
