de82b554445c179fcd65ab7cb5cadfd58603f575
Baljeet Sandhu <Baljeet.mail@gmail.com>
2016-09-10 16:51:05 -0400

    Clean re-deploy

--- deployToDigitalOcean.sh
+++ deployToDigitalOcean.sh
@@ -7,7 +7,6 @@ ssh root@45.55.192.142 'rm -rf ~/epitomicweb'
 ssh root@45.55.192.142 'mkdir ~/epitomicweb'
 scp * root@45.55.192.142:~/epitomicweb;
 scp -rC server root@45.55.192.142:~/epitomicweb;
-scp -rC site root@45.55.192.142:~/epitomicweb;
 
 echo""
 echo "*** Done ***"
